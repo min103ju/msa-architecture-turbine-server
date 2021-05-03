bfb3ca3053a7cea7cb9d8c6a607dc4873fb63aeb
Min Heo <min103ju@gmail.com>
2021-05-03 21:28:51 +0900

    Initial commit

---
+++ src/main/java/com/citizen/msa/turbine/MsaTurbineApplication.java
@@ -0,0 +1,15 @@
+package com.citizen.msa.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableTurbine
+public class MsaTurbineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsaTurbineApplication.class, args);
+    }
+}
